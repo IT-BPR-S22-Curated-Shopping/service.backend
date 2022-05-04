fd949b8641e5f784daf6b70b8a5aef83f4f01961
Stygg <48440436+VIA-Jesper@users.noreply.github.com>
2022-05-04 08:50:07 +0200

    repository update

--- src/main/java/bpr/service/backend/models/sql/CustomerEntity.java
+++ src/main/java/bpr/service/backend/models/sql/CustomerEntity.java
@@ -22,8 +22,4 @@ public class CustomerEntity implements Serializable {
 
     public CustomerEntity() {
     }
-
-
-
-
 }
--- src/main/java/bpr/service/backend/services/sql/ICustomerRepository.java
+++ src/main/java/bpr/service/backend/services/sql/ICustomerRepository.java
@@ -5,11 +5,9 @@ import bpr.service.backend.models.sql.CustomerEntity;
 import java.util.Optional;
 
 public interface ICustomerRepository {
+
     CustomerEntity addCustomer(CustomerEntity customer);
-
     Iterable<CustomerEntity> findAllCustomers();
-
     CustomerEntity findCustomerByUUID(String uuid);
-
     Optional<CustomerEntity> findCustomerById(Long id);
 }
